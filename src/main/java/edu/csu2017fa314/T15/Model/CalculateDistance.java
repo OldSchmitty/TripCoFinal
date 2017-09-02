ae821b6b107b96b5bc2339a5f60d111e83312f6a
Josiah May <Josiah@pleasefixit.com>
2017-09-02 15:25:21 -0600

    Add CalculateDistance.java and CalculateDistanceTest.java

---
+++ src/main/java/edu/csu2017fa314/T15/Model/CalculateDistance.java
@@ -0,0 +1,10 @@
+package edu.csu2017fa314.T15.Model;
+
+/**
+ * <p>Calculates the distance from two latitude and longitude points</p>
+ * @author jmay
+ * @version 1.0 -9/2/17 - initial
+ */
+public class CalculateDistance {
+
+}
