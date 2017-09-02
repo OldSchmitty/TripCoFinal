9f348d38b00d6395f84fc4541c50e05a95728a2f
Josiah May <Josiah@pleasefixit.com>
2017-09-02 14:37:08 -0600

    Added Brewery.java and BreweryTest.java Added BreweryTest to Alltest for maven to check.

---
+++ src/main/java/edu/csu2017fa314/T15/Model/Brewery.java
@@ -0,0 +1,20 @@
+package edu.csu2017fa314.T15.Model;
+
+/**
+ * Class that holds all the info on a brewery
+ */
+public class Brewery {
+
+   /**
+    * Default constructor for Brewery class
+    *
+    */
+   Brewery(){
+     /*
+      * TODO When fully constructed set ID-test, NAME-Test Brewery, CITY-FORT COLLINS,
+      * TODO LATITIUDE- 40&deg; 35' 6.92" N, LONGITUDE - 105&deg; 5' 3.9084" W, ELVEVATION - 5007,
+      * TODO destination -"", distance - 0;
+      */
+
+   }
+}
---
+++ src/test/java/edu/csu2017fa314/T15/Model/BreweryTest.java
@@ -0,0 +1,22 @@
+package edu.csu2017fa314.T15.Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BreweryTest {
+
+  private Brewery b;
+  @Before
+  public void setUp() throws Exception {
+    b = new Brewery();
+  }
+
+  @Test
+  public void testsImport() throws Exception {
+    b = new Brewery();
+  }
+
+}
