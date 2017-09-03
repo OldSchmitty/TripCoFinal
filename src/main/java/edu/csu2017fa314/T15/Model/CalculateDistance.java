abb15ca049866ac4f2ab519e9382dee63591b2a8
Josiah May <Josiah@pleasefixit.com>
2017-09-03 09:33:03 -0600

    Parse the string

--- src/main/java/edu/csu2017fa314/T15/Model/CalculateDistance.java
+++ src/main/java/edu/csu2017fa314/T15/Model/CalculateDistance.java
@@ -7,4 +7,21 @@ package edu.csu2017fa314.T15.Model;
  */
 public class CalculateDistance {
 
+  public static double stringToDoubeForCoordinate (final String coordinate){
+
+    String str = coordinate.toLowerCase();
+    int mod = 1;
+    if(str.endsWith("s") || str.endsWith("w") ) {
+      mod = -1;
+    }
+
+    String[] stringElements = str.replaceAll("[^-0-9\\.]+", " ").split(" ");
+    Double[] doubleElements = new Double[stringElements.length];
+
+    for (int i = 0; i < stringElements.length; i++) {
+      doubleElements[i] = Double.parseDouble(stringElements[i]);
+    }
+
+    return 0;
+  }
 }
