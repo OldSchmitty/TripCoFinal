630fafb9d7b2ca7511892cd3a997c6982a2adfb0
James Depoyster <jdepoiii@atlanta.cs.colostate.edu>
2017-09-03 13:34:32 -0600

    closes #54

--- src/main/java/edu/csu2017fa314/T15/Model/Model.java
+++ src/main/java/edu/csu2017fa314/T15/Model/Model.java
@@ -1,28 +1,35 @@
 package edu.csu2017fa314.T15.Model;
 
+import java.util.ArrayList;
+
 public class Model
-
 {
+   private String[] inputdata;
+   private  ArrayList<Brewery> breweries;
+   
+   /* fields will allow the order and position of the relevant field columns to be determined dynamically, and will be used to extract the necassary data, 
+    i.e. fields[0] will represent ID, and might be in position 2, thus fields[0] = 2; */  
+   private int[] fields;
+   
+   public enum Data {
+	    ID, NAME, CITY, LATITUDE, LONGITUDE, ELEVATION
+	}
 
-   private int[] numbers;
-
-   public Model ()
+   public Model () 
    {
+     
+   }
 
-      numbers = new int[] {0, 1, 2, 3, 4, 5};
+   public void readFile(String filename){
 
    }
 
-
-
-   public int[] getNumbers() 
-
-   {
-
-      return numbers;
-
+   private void storeInfo(){
+	   
    }
-
-
-
+   
+   public Brewery getBrewery(String name){
+	return null;
+   }
 }
+
