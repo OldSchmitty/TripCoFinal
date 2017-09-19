c9a1ba79a8eaffa2fce10224fdddce6c0483cf1e
Josiah May <Josiah@pleasefixit.com>
2017-09-19 15:49:20 -0600

    Add basic constructor for new logic

--- src/main/java/edu/csu2017fa314/T15/View/View.java
+++ src/main/java/edu/csu2017fa314/T15/View/View.java
@@ -1,16 +1,36 @@
 package edu.csu2017fa314.T15.View;
+
+import edu.csu2017fa314.T15.Model.Brewery;
+import edu.csu2017fa314.T15.Model.Edge;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class View
 {
-   private int totalDistance;
+  private HashMap<String, Brewery> dest;
+  private ArrayList<Edge> edge;
 
-   public void setTotalDistance(int distance) 
-   {
-       totalDistance = distance; 
-   }
+  public View (final HashMap<String, Brewery> dest, final ArrayList<Edge> edge, final String path){
+    this.dest = dest;
+    this.edge = edge;
+    run();
+  }
 
-   public int getTotalDistance() 
-   {
-      return totalDistance;
-   }
+  public View(){
+  }
+  private void run(){
+
+  }
+  private int totalDistance;
+
+  public void setTotalDistance(int distance)
+  {
+     totalDistance = distance;
+  }
+
+  public int getTotalDistance()
+  {
+    return totalDistance;
+  }
 
 }
