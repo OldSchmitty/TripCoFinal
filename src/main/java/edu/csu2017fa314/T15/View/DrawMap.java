58dea9d362c4c045cec6e5f1afa39fc2b4d66c64
Josiah May <Josiah@pleasefixit.com>
2017-09-21 22:07:55 -0600

    Add logic to make Start and end of svg file

--- src/main/java/edu/csu2017fa314/T15/View/DrawMap.java
+++ src/main/java/edu/csu2017fa314/T15/View/DrawMap.java
@@ -1,17 +1,35 @@
 package edu.csu2017fa314.T15.View;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class DrawMap {
+
+  private String path; // Dir to make file
+  private ArrayList<String> elements; // What to write
 
 
   /**
-   * Initializes elements to draw SVG and make the file
-   * at the given path
+   * <p>Initializes elements to draw SVG</p>
    * @param path - where to build file
    */
   public DrawMap(final String path){
-
+    this.path = path;
+    this.elements = new ArrayList<>();
+    svgHeader();
   }
 
+  private void svgHeader(){
+    String head = new String("<?xml version=\"1.0\"?>\n"
+        + "<svg width=\"1066.6073\" height=\"783.0824\" xmlns=\"http://www.w3.org/2000/svg\">\n");
+    elements.add(head);
+  }
   /**
    * Draws the boundaries of Colorado
    */
@@ -42,6 +60,22 @@ public class DrawMap {
    * Writes the data to the svg file
    */
   public void write(){
+
+    Charset charset = Charset.forName("US-ASCII");
+
+    try
+    {
+      BufferedWriter writer = Files.newBufferedWriter( Paths.get(path),
+          charset);
+      writer.write(elements.get(0));
+      writer.write("\n</svg>");
+      writer.close();
+
+    } catch (java.io.IOException e ) {
+      throw new RuntimeException("Error in DrawMap.write() opening file", e);
+
+    }
+
 
   }
 
