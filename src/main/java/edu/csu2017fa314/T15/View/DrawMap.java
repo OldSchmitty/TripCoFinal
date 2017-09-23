83f789d0f7823905c1e7975435cce0a6c7675d9c
Josiah May <Josiah@pleasefixit.com>
2017-09-23 16:30:02 -0600

    Basic Logic for drawPath

--- src/main/java/edu/csu2017fa314/T15/View/DrawMap.java
+++ src/main/java/edu/csu2017fa314/T15/View/DrawMap.java
@@ -1,18 +1,17 @@
 package edu.csu2017fa314.T15.View;
 
+import edu.csu2017fa314.T15.Model.CalculateDistance;
 import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class DrawMap {
 
   private String path; // Dir to make file
   private ArrayList<String> elements; // What to write
+  private int edgeLoc = 0; //What element[i] the edges are at
 
   /**
    * <p>Initializes elements to draw SVG</p>
@@ -36,12 +35,11 @@ public class DrawMap {
    * Draws the boundaries of Colorado
    */
   public void drawColorado(){
-    String mapElem = " <g>\n  <title>ColoradoBorders</title>\n  <!-- Draw Colorado -->\n";
-    //All Code from https://svg-edit.github.io/svgedit
-    mapElem += "<rect id=\"Colorado\" height=\"708.68515\" width=\"991.4014\" y=\"37.4016\" "
+    String mapElem = "  <title>ColoradoBorders</title>\n  <!-- Draw Colorado -->\n";
+    //All SVG Code from https://svg-edit.github.io/svgedit
+    mapElem += "  <rect id=\"Colorado\" height=\"708.68515\" width=\"991.4014\" y=\"37.4016\" "
         + "x=\"37.52397\" stroke-linecap=\"null\" stroke-linejoin=\"null\" "
         + "stroke-dasharray=\"null\" stroke-width=\"5\" stroke=\"#000000\" fill=\"none\"/>";
-    mapElem += " </g>";
     elements.add(mapElem);
   }
 
@@ -50,8 +48,26 @@ public class DrawMap {
    * @param lat - Latitude of destination
    * @param lon - Longitude of destination
    */
-  public void addPoint(final String lat, final String lon){
+  public void addPathPoint(final String lat, final String lon){
+    //All SVG Code from https://svg-edit.github.io/svgedit
+    if(edgeLoc == 0) // add start of path logic
+    {
+      edgeLoc = elements.size();
+      String pathSetup = "  <title>RoutePath</title>\n  <!-- Draw The path -->\n";
+      pathSetup += "  <path id=\"svg_3\" fill=\"none\" stroke=\"#00007f\" stroke-width=\"3\" "
+          + "stroke-dasharray=\"null\" stroke-linejoin=\"null\" stroke-linecap=\"null\" d=\"";
+      elements.add(edgeLoc, pathSetup);
+    }
+    String add = elements.get(edgeLoc);
+    add += "l"+CalculateDistance.stringToDoubleForCoordinate(lat) + ","
+        + CalculateDistance.stringToDoubleForCoordinate(lon);
+    elements.set(edgeLoc,add);
+  }
 
+  private void finishPath(){
+    if (edgeLoc !=0){
+      elements.set(edgeLoc, elements.get(edgeLoc) +"\"/>");
+    }
   }
 
   /**
@@ -75,8 +91,15 @@ public class DrawMap {
     {
       BufferedWriter writer = Files.newBufferedWriter( Paths.get(path),
           charset);
-      for (String elm : elements){
-        writer.write(elm);
+
+      finishPath();
+
+      writer.write(elements.get(0)); // Header file
+      // Write all elements
+      for (int i = 1; i < elements.size(); i++){
+        writer.write( " <g>\n");
+        writer.write(elements.get(i));
+        writer.write("\n </g>\n");
       }
 
       writer.write("\n</svg>");
