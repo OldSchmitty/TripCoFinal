d81e571d9df19a65c607b19fc60c211ed1d488ba
Josiah May <Josiah@pleasefixit.com>
2017-09-26 16:21:42 -0600

    Changed TripCo logic

--- src/main/java/edu/csu2017fa314/T15/TripCo.java
+++ src/main/java/edu/csu2017fa314/T15/TripCo.java
@@ -1,40 +1,35 @@
 package edu.csu2017fa314.T15;
 
+import edu.csu2017fa314.T15.Model.Edge;
+import edu.csu2017fa314.T15.Model.Itinerary;
 import edu.csu2017fa314.T15.Model.Model;
 import edu.csu2017fa314.T15.Model.CalculateDistance;
 import edu.csu2017fa314.T15.Model.Destination;
 import edu.csu2017fa314.T15.View.JSONWriter;
 
+import edu.csu2017fa314.T15.View.View;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.util.ArrayList;
+import org.omg.IOP.CodecPackage.InvalidTypeForEncoding;
+
 public class TripCo
 {
 
 	private static Model m;
 	private static CalculateDistance calc;
 	private static JSONWriter jw;
-	
+	private static String path = "." + File.separator + "data" + File.separator;
 	
    public static void main(String[] args) throws FileNotFoundException {
-	   m = new Model();
-	   jw = new JSONWriter("." + File.separator + "data" + File.separator + "BreweryItinerary.json");
-	   m.readFile(args[0]);
-	   
-	   Destination first = new Destination();
-	   Destination second = new Destination();
-	   long distance;
-	   
-	   for(int i = 0; i < m.getSize()-1; i++) {
-		   if(i == 0)
-			   first = m.getNextBrewery();
-		   else
-			   first = second;
-		   second = m.getNextBrewery();
-		   distance = calc.findDistanceBetween(first, second);
-		   jw.add(first.getId(), second.getId(), distance);
-	   }
-	   
-	   jw.write(); jw.close();
+	   m = new Model(args[0]);
+
+		 Itinerary i = new Itinerary(m.getMap());
+		 ArrayList<Edge> edges = i.getShortestPath();
+		 View v = new View(path);
+		 v.makeItinerary(edges);
+		 v.makeDestination(m.getMap());
+		 v.drawMap(m.getMap(), edges);
    }
 
 }
