5af6b531138ed5da34f41fb6257ede43da9ac4c2
jhdepoiii <jhdepoiii@gmail.com>
2017-11-01 13:18:13 -0600

    Fixes bug caused by removing the test_output directory (it wasn't letting me commit because of a file in there)

--- src/main/java/edu/csu2017fa314/T15/Model/NearestNeighbor.java
+++ src/main/java/edu/csu2017fa314/T15/Model/NearestNeighbor.java
@@ -4,15 +4,27 @@ import java.util.ArrayList;
 
 public class NearestNeighbor {
 
+    // reference to the global distanceTable and the currentDistance of our optimized path
     private Table distanceTable;
     private long currentDistance;
 
 
+    /**
+     * initialize the distanceTable and currentDistance
+     * @param distanceTable
+     * @param currentDistance
+     */
     public NearestNeighbor(Table distanceTable, long currentDistance){
         this.distanceTable = distanceTable;
         this.currentDistance = currentDistance;
     }
 
+
+    /**
+     * compute the nearestNeighbor of the current node from the set of all other nodes yet to be used
+     * @param id
+     * @param remainingKeys
+     */
     public Integer nearestNeighbor(Integer id, ArrayList<Integer> remainingKeys){
         Integer nearest = -1;
         long nearDist = -1;
