75e44170f44fc8885a6a4d0059cad68ba2011660
Josiah May <josiahm@rams.colostate.edu>
2017-11-03 09:44:17 -0600

    When building the table it was only in km or miles so one of the tests will fail Also total distance in km should be more then miles

--- src/test/java/edu/csu2017fa314/T15/Model/TableTest.java
+++ src/test/java/edu/csu2017fa314/T15/Model/TableTest.java
@@ -1,17 +1,38 @@
 package edu.csu2017fa314.T15.Model;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import java.util.HashMap;
 import static org.junit.Assert.*;
 
 public class TableTest {
 
-    private Destination[] map;
+    static private Destination[] map;
     private Table table;
 
+    @BeforeClass
+    static public void destinationsSetup()
+    {
+        Destination b1 = new Destination("a", "Two22 Brew", "Centennial", "39°38'07\" N",
+            "104°45'32\" W", "5872");
+        b1.setIdentifier(0);
+        Destination b2 = new Destination("b", "Equinox Brewing", "Fort Collins", "40°35'17\" N",
+            "105°4'26\" W", "4988");
+        b2.setIdentifier(1);
+        Destination b3 = new Destination("c", "Mad Jacks Mountain Brewing", "Bailey", "39°24'05\" N",
+            "105°28'37\" W", "9580");
+        b3.setIdentifier(2);
+
+        map = new Destination[3];
+        map[0] = b1;
+        map[1] = b2;
+        map[2] = b3;
+    }
+    /*
     @Before
     public void setup() throws Exception{
+        CalculateDistance.setKilometers();
         Destination b1 = new Destination("a", "Two22 Brew", "Centennial", "39°38'07\" N",
                 "104°45'32\" W", "5872");
         b1.setIdentifier(0);
@@ -28,7 +49,7 @@ public class TableTest {
         map[2] = b3;
 
         table = new Table(map);
-    }
+    }*/
 
     /**
      * A few tests to assert that the Table constructor builds properly and the getEdge method works
@@ -36,12 +57,14 @@ public class TableTest {
     @Test
     public void test1() {
         CalculateDistance.setMiles();
-        assertEquals(table.getDistance(0, 1), 109);
+        table = new Table(map);
+        assertEquals(table.getDistance(0, 1), 68);
     }
 
     @Test
     public void test2() {
         CalculateDistance.setKilometers();
-        assertEquals(table.getDistance(1, 0), 68);
+        table = new Table(map);
+        assertEquals(table.getDistance(1, 0), 109);
     }
 }
