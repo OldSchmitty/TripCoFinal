23031ba97fa8a0f9fb3e2241d926653668dcee7b
awdennis <andrew.w.dennis@gmail.com>
2017-11-08 14:23:18 -0700

    Adds testMode to Three Opt  * enableTestMode(int) - indicates which case is tested  * disableTestMode() - reverts 3-opt from testMode

--- src/main/java/edu/csu2017fa314/T15/Model/ThreeOpt.java
+++ src/main/java/edu/csu2017fa314/T15/Model/ThreeOpt.java
@@ -4,6 +4,8 @@ public class ThreeOpt {
 
     private Table distanceTable;    // table for calculating distances between two destinations
     private Integer[] route;        // current, optimized route. Changes are made in place
+    private boolean testMode;       // flag for testing 3-Opt
+    private int optCase;            //
 
     /**
      * Constructor
@@ -11,5 +13,28 @@ public class ThreeOpt {
     ThreeOpt(Table distance, Integer[] path) {
         this.distanceTable = distance;
         this.route = path;
+        this.testMode = false;
     }
+
+
+
+
+    /**
+     * Enables test mode, allowing specific cases to be selected.
+     * optCase is the 3-Opt case to be tested
+     */
+    public void enableTestMode(int optCase){
+        this.optCase = optCase;
+        this.testMode = true;
+    }
+
+    /**
+     * Disables test mode, reverting it back to default so all 3-Opt cases are used
+     */
+    public void disableTestMode(){
+        this.testMode = false;
+    }
+
+
+
 }
