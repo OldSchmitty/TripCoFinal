66a099fe88512c9a2b4dc1afe76cec13f1e847c2
jhdepoiii <jhdepoiii@gmail.com>
2017-11-17 16:12:28 -0700

    Removes redundant test @BeforeClass already performed in ItinenaryTest

--- src/test/java/edu/csu2017fa314/T15/Model/NearestNeighborTest.java
+++ src/test/java/edu/csu2017fa314/T15/Model/NearestNeighborTest.java
@@ -11,22 +11,7 @@ public class NearestNeighborTest {
 
     private static Destination[] map;
     private static Itinerary itinerary;
-
-    @BeforeClass
-    public static void setup() throws Exception{
-        Destination b1 = new Destination("0", "Destination 1", "Test City 1", "02", "00", "1000");
-        b1.setIdentifier(0);
-        Destination b2 = new Destination("1", "Destination 2", "Test City 2", "04", "00", "1000");
-        b2.setIdentifier(1);
-        Destination b4 = new Destination("3", "Destination 4", "Test City 4", "01", "00", "1000");
-        b4.setIdentifier(2);
-        Destination b5 = new Destination("4", "Destination 5", "Test City 5", "08", "00", "1000");
-        b5.setIdentifier(3);
-
-        map =  new Destination[] {b1,b2,b4,b5};
-        itinerary = new Itinerary(map, "Nearest Neighbor");
-        itinerary.getShortestPath();
-    }
+    
 
     private long testItinerary(String path){
 
