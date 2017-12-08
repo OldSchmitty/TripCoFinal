9948e8159eef299aa2719310941a9ef8486286b9
OldSchmitty <18094250+OldSchmitty@users.noreply.github.com>
2017-12-07 18:15:16 -0700

    "Component for the Save button for kml file. closes #372 closes #358"

---
+++ web/src/components/Home/SaveKML/SaveKML.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import ButtonToolbar from "react-bootstrap/es/ButtonToolbar";
+class SaveKML extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            KMLFile: this.props.KMLFile
+        };
+    }
+
+    render() {
+        return (
+            <div className="saveload">
+                <ButtonToolbar className="SaveKML">
+                    <button type="button" onClick={this.saveButtonClicked.bind(this)}>
+                        Save Trip as KML File
+                    </button>
+                </ButtonToolbar>
+            </div>
+        )
+    }
+
+    saveButtonClicked(event) {
+        this.getFile();
+    }
+
+    async getFile() {
+        if(this.state.KMLFile !== '') {
+            let pom = document.createElement('a');
+            pom.setAttribute('href', 'data:text/plain;charset=utf-8,'
+                + encodeURIComponent(this.state.KMLFile));
+            pom.setAttribute('download', "TripCoTrip.kml");
+
+            if (document.createEvent) {
+                let event = document.createEvent('MouseEvents');
+                event.initEvent('click', true, true);
+                pom.dispatchEvent(event);
+            } else {
+                pom.click();
+            }
+
+            if (pom.parentNode) {
+                pom.parentNode.removeChild(pom);
+            }
+        }
+    }
+
+}
+
+export default SaveKML;
